4ec3124b9c43b64eea7fdea4566dfba2e2b9fe64
alexmaggard <amaggard09@gmail.com>
2015-12-16 09:15:55 -0800

    Update CarDemo.java
    I attempted to put the objects into an array list to get a read out of everything.. after looking at the requirements again I had to remove the array list.. But still not understanding how to read out all subclasses using the Car object.

--- src/carPack/CarDemo.java
+++ src/carPack/CarDemo.java
@@ -8,28 +8,16 @@ import java.util.Collection;
 	 */
 	public class CarDemo
 	{   
-	    
-			static ArrayList<Car> CarCompare = new ArrayList<Car>();
-	    
-	    static GasolineCar Civic;
-	    static ElectricCar Fuze;
-	    static HybridCar Optima;
-	    static GasolineCar Mustang;
-	    static ElectricCar Tesla;
-	    static HybridCar Fusion;          
-	    
-	    
-	    @SuppressWarnings("unchecked")
-			public static void main(String[] args)
+
+	public static void main(String[] args)
 	    {   
-	        CarCompare.addAll((Collection<? extends Car>) (Civic =   new GasolineCar("Honda", "Gasoline",2015, "XY3234", 18)));
-	        CarCompare.addAll((Collection<? extends Car>) (Fuze  =   new ElectricCar("Chevy", "Electric", 2015, "XI25803", 15)));
-	        CarCompare.addAll((Collection<? extends Car>) (Optima =  new HybridCar("Kia", "Hybrid", 2015, "AF24346", 18, 15)));
-	        CarCompare.addAll((Collection<? extends Car>) (Mustang = new GasolineCar("Ford", "Gasoline",2015, "76298374", 14)));
-	        CarCompare.addAll((Collection<? extends Car>) (Tesla =   new ElectricCar("Tesla", "Electric", 2015, "TT23422", 18)));
-	        CarCompare.addAll((Collection<? extends Car>) (Fusion =  new HybridCar("Ford", "Hybrid", 2015, "FF23224", 18, 15)));
-	       
-	      System.out.println(CarCompare); // Null is read out 6 times..
+	        Civic =   new GasolineCar("Honda", "Gasoline",2015, "XY3234", 18)));
+	        Fuze  =   new ElectricCar("Chevy", "Electric", 2015, "XI25803", 15)));
+	        Optima =  new HybridCar("Kia", "Hybrid", 2015, "AF24346", 18, 15)));
+	        Mustang = new GasolineCar("Ford", "Gasoline",2015, "76298374", 14)));
+	        Tesla =   new ElectricCar("Tesla", "Electric", 2015, "TT23422", 18)));
+	        Fusion =  new HybridCar("Ford", "Hybrid", 2015, "FF23224", 18, 15)));
+
 	       
 	    }
 
